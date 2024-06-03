3d04baab4254c9b5e70d3ee48c4e661918476148
jayb989898 <giacomofracassi47@gmail.com>
2024-06-03 07:25:51 +0200

    installed heroicons, add new components for show lifes

--- guess-the-word.web-ui/src/routes/quiz/quiz.tsx
+++ guess-the-word.web-ui/src/routes/quiz/quiz.tsx
@@ -1,15 +1,31 @@
 import "./quiz.scss";
 import { useEffect, useState } from "react";
+import HeartsList from "../../components/hearts-list/hearts-list";
 
 export default function Quiz() {
-  const text: string = "abcadadeqwdfe";
+  const totalLifes: number = 5;
+  const text: string = "abcd";
   const textArr: Array<string> = text.split("");
   const [focusInput, setFocusInput] = useState(0);
   const [inputs, setInputs] = useState(Array(text.length).join(".").split("."));
+  const [testPassed, setTestPassed] = useState(false);
+  const [remainingLifes, setLifesRemaining] = useState(totalLifes);
 
   useEffect(() => {
     selectInput(focusInput);
+    checkTestPassed();
   });
+
+  function selectInput(index: number) {
+    const input = document.getElementById(index.toString());
+    input?.focus();
+  }
+
+  function checkTestPassed() {
+    if (textArr.toString() === inputs.toString()) {
+      setTestPassed(true);
+    }
+  }
 
   let inputsComponent = () => {
     return textArr?.map((char: string, index: number) => {
@@ -22,7 +38,9 @@ export default function Quiz() {
             type="text"
             maxLength={1}
             value={inputs[index]}
-            onChange={(e) => checkInputValue(index, e.target.value)}
+            onChange={(e) => {
+              checkInputValue(index, e.target.value);
+            }}
             disabled={inputs[index] !== ""}
           />
         </>
@@ -37,20 +55,21 @@ export default function Quiz() {
       );
       setFocusInput(index + 1);
       setInputs(input);
+    } else {
+      setLifesRemaining(remainingLifes - 1);
     }
-  }
-
-  function selectInput(index: number) {
-    const input = document.getElementById(index.toString());
-    input?.focus();
   }
 
   return (
     <div id="container-quiz">
       <div className="grid h-screen place-items-center">
-        <div className="rounded overflow-hidden shadow-lg bg-black h-1/2 min-w-96 max-w-3xl">
-          <div className="px-6 py-4 h-4/5">
-            <div className="font-bold text-xl mb-2">Guess the word</div>
+        <div className="rounded overflow-hidden shadow-lg bg-black h-1/2 min-h-96 min-w-96 max-w-3xl">
+          <div className="px-6 py-4 h-full">
+            <div className="font-bold text-xl mb-8">
+              <p className="flex text-fuchsia-700 content-center justify-center">
+                Guess the word
+              </p>
+            </div>
             <div className="h-1/4 rounded bg-slate-600">
               <div className="size-full flex">
                 <div className="m-auto">
@@ -59,12 +78,28 @@ export default function Quiz() {
                   </span>
                 </div>
               </div>
+              <HeartsList
+                totalLifes={totalLifes}
+                remainingLifes={remainingLifes}
+              />
             </div>
-            <form className="h-3/4 w-full pt-2">
-              <div className="flex mx-3 content-center justify-between">
+            <form className="h-2/4 w-full pt-2">
+              <div className="flex mx-3 content-center justify-between pt-14">
                 {inputsComponent()}
               </div>
             </form>
+            <div className="flex content-center justify-center">
+              <button
+                disabled={!testPassed}
+                className={`${
+                  testPassed
+                    ? "bg-fuchsia-500 hover:bg-fuchsia-700"
+                    : "bg-fuchsia-900"
+                } text-white font-bold py-2 px-4 rounded`}
+              >
+                Button
+              </button>
+            </div>
           </div>
         </div>
       </div>
