ca00e9e8568cb8a38742f8b745e7b415bbf74c78
jayb989898 <giacomofracassi47@gmail.com>
2024-06-10 19:05:47 +0200

    creation subfolders client/server + add .NET

---
+++ Server/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Guess_the_word
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
